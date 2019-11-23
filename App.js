a41cc916680928fd0230dede9981f1e8a42054e2
Gabriel Ikezaki <gabriel.mori@live.com>
2019-11-23 16:58:55 -0300

    animacoes de posicao e drag esta funcionando pra primeira imagem na pilha

--- App.js
+++ App.js
@@ -15,23 +15,56 @@ const  Profile = [
 
 export default class App extends React.Component{
   
+  constructor(){
+    super();
+    this.position = new Animated.ValueXY();
+    //garantindo q apenas a primeira imagem da pilha tera sua posicao alterada 
+    this.state = {
+      currentIndex: 0
+    }
+  }
+
+  //Esse metodo sera responsavel por criar um objeto, que sera atribuido ao componente antes de renderizar a lista de imagens
+  componentWillMount(){
+    this.PanResponder = PanResponder.create({
+     
+     //se esse evento ocorrer, os valores x e y da imagens serao mudados no setValue
+      onStartShouldSetPanResponder:(evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+          this.position.setValue({x: gestureState.dx, y: gestureState.dy});
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        
+      }
+    })
+  }
+
 
   //essa funcao vai renderizar todas as imagens na array Profile
   //Essa funcao sera chamada na funcao render (onde usamos o flex para ajustar o tamanho das imagens)
   //Enquanto a funcao renderprofile renderiza as imagens em si, a funcao render vaiajustar o tamanho de cada imagem
   renderProfile = () =>{
     return Profile.map((item, i) => {
-      return(
-        <Animated.View
-        key={i}
-        style={[
-          {
-            height: SCREEN_HEIGHT - 120,
-            width: SCREEN_WIDTH,
-            padding: 10,
-            position:'absolute'
-          }
-        ]}
+
+      if(i < this.state.currentIndex)
+      {
+        return null;
+      }
+      else if(i == this.state.currentIndex)
+      {
+        return(
+          <Animated.View
+          {...this.PanResponder.panHandlers}
+          key={item.id}
+          style={[
+            {transform: this.position.getTranslateTransform()},
+            {
+              height: SCREEN_HEIGHT - 120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position:'absolute'
+            }
+          ]}
       >
         <Image
           style={{
@@ -46,8 +79,39 @@ export default class App extends React.Component{
         
         </Animated.View>
       )
-    })
-  }
+    }
+    else
+    {
+      alert ("else");
+      return(
+        <Animated.View
+          key={item.id}
+          style={[
+            {
+              height: SCREEN_HEIGHT -120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: 'absolute'
+            }
+          ]}
+        >
+            <Image
+              style={{
+                flex: 1,
+                height: null,
+                width: null,
+                resizeMode: "cover",
+                borderRadius: 20
+              }}
+                source={item.uri}
+              />
+          </Animated.View>
+         
+      )
+    }
+  }).reverse();
+}
+
 
   //O codigo abaixo, sendo uma classe, vai renderizar as dimensões das imagens
   //Por isso, vamos usar o flex para ajustar esses valores 
